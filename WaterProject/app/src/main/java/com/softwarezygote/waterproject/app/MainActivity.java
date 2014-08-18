bac3a7a1de90085d6759efb85cbcacc71957da16
christopherHays <christopherhays@outlook.com>
2014-08-18 12:59:05 -0700

    New Layout, DB Complete
    Layout is locked horizontal, DB is done. Still needs an options menu.

--- WaterProject/app/src/main/java/com/softwarezygote/waterproject/app/MainActivity.java
+++ WaterProject/app/src/main/java/com/softwarezygote/waterproject/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.softwarezygote.waterproject.app;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -14,69 +15,78 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.ScrollView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
     // has the same methods as ActionBarActivity, they can be overridden if needed
 
     // Variables
-    TextView resultString;  // correct or incorrect
-    TextView ques;  // the question field
-    TextView a;  // answer a field
-    TextView b;  // answer b field
-    TextView c;  // answer c field
-    TextView d;  // answer d field
-    RadioGroup radioGrp;  // radio button field
-    boolean result = false;  // was the guess correct?
-    int answer = 0;  // the position of the correct answer (0-3)
-    Button mainButton;  // submit answer button handler
-    Button nextButton;  // next question button handler
-    int dbId = 0;  // current database row
-    long dbLength;  // length of the database
-    DatabaseHelper db;  // database object
-    Question currentQuestion;  // question object
+    ScrollView s;                       // ScrollView to control the scroll
+    ScrollView s2;
+    TextView resultString;              // correct or incorrect
+    TextView ques;                      // the question field
+    TextView a;                         // answer a field
+    TextView b;                         // answer b field
+    TextView c;                         // answer c field
+    TextView d;                         // answer d field
+    RadioGroup radioGrp;                // radio button field
+    boolean result = false;             // was the guess correct?
+    int answer = 0;                     // the position of the correct answer (0-3)
+    Button mainButton;                  // submit answer button handler
+    Button nextButton;                  // next question button handler
+    int dbId = 0;                       // current database row
+    long dbLength;                      // length of the database
+    DatabaseHelper db;                  // database object
+    Question currentQuestion;           // question object
 
     // onCreate
     @Override  // override the extended onCreate
-    protected void onCreate(Bundle savedInstanceState) {  // first thing the app does
-        super.onCreate(savedInstanceState);  // calls the parent onCreate as well as the new one
-        setContentView(R.layout.activity_main);  // set the layout according to the xml
+    protected void onCreate(Bundle savedInstanceState) {        // first thing the app does
+        super.onCreate(savedInstanceState);                     // calls the parent onCreate as well as the new one
 
-        // variable initialization
-        currentQuestion = new Question();  // finish creating the question object
-        db = new DatabaseHelper(this);  // finish creating the database object, send this context
+        setContentView(R.layout.activity_main);                 // set the layout according to the xml
+
+        currentQuestion = new Question();                       // finish creating the question object
+        db = new DatabaseHelper(this);                          // finish creating the database object, send this context
 
         // set the radio button group
-        radioGrp = (RadioGroup)findViewById(R.id.radios); // find by xml id
+        radioGrp = (RadioGroup)findViewById(R.id.radios);       // find by xml id
 
         // database create and open
-        db.createDatabase();  // call create method on database object
-        db.openDatabase();  // call open method
+        db.createDatabase();                                    // call create method on database object
+        db.openDatabase();                                      // call open method
 
         // find the length of the database
-        dbLength = db.count();  // call count method
+        dbLength = db.count();                                  // call count method
 
         // set the current question
-        currentQuestion = update(dbId, db);  // call update method to populate currentQuestion
+        currentQuestion = update(dbId, db);                     // call update method to populate currentQuestion
 
         // set onClick listeners
-        mainButton = (Button) findViewById(R.id.main_button);  // find id from xml
-        mainButton.setOnClickListener(this);  // listen for a button press in this context
-        nextButton = (Button) findViewById(R.id.next_button);  // find id from xml
-        nextButton.setOnClickListener(this);  // listen for a button press in this context
+        mainButton = (Button) findViewById(R.id.main_button);   // find id from xml
+        mainButton.setOnClickListener(this);                    // listen for a button press in this context
+        nextButton = (Button) findViewById(R.id.next_button);   // find id from xml
+        nextButton.setOnClickListener(this);                    // listen for a button press in this context
 
         // set the textViews
-        ques = (TextView) findViewById(R.id.questions);  // reference xml
-        a = (TextView) findViewById(R.id.radio_A);  // reference xml
-        b = (TextView) findViewById(R.id.radio_B);  // reference xml
-        c = (TextView) findViewById(R.id.radio_C);  // reference xml
-        d = (TextView) findViewById(R.id.radio_D);  // reference xml
+        ques = (TextView) findViewById(R.id.questions);         // reference xml
+        a = (TextView) findViewById(R.id.radio_A);
+        b = (TextView) findViewById(R.id.radio_B);
+        c = (TextView) findViewById(R.id.radio_C);
+        d = (TextView) findViewById(R.id.radio_D);
+        s = (ScrollView) findViewById(R.id.scroll);
+        s2 = (ScrollView) findViewById(R.id.scroll2);
 
         // display the text
-        display();  // call display method
+        display();                  // call display method
 
     // end of onCreate
     }
+
+
 
     // updates the current question
     public Question update(int id, DatabaseHelper d){  // two parameters
@@ -160,27 +170,32 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
             resultString = (TextView) findViewById(R.id.display_result);  // reference xml
         switch(view.getId()) {  // switch to see which button it was
             case R.id.main_button:  // main case
-                if (result)  // if boolean result is true
+                if (result) {  // if boolean result is true
                     resultString.setText("CORRECT!");  // set the text
-                else
+                    resultString.setBackgroundColor(0xFF66FF99);
+                }
+                else {
                     resultString.setText("INCORRECT.");
+                    resultString.setBackgroundColor(0xFFFF8484);
+                }
                 break;
             case R.id.next_button:  // next question case, also resets results
-                dbId += 1;  // increment the database row counter
+
+                dbId = (int )(Math.random() * (dbLength-1));
+                //dbId += 1;  // increment the database row counter
+
                 if(dbId >= dbLength){  // if the row counter exceeds the size of the database
                     dbId = 0;  // reset the row counter
                 }
                 currentQuestion = update(dbId, db);  // grab a new question from the database
                 display();  // display the new question
+                s.smoothScrollTo(0,0);  // scroll back to the top
+                s2.smoothScrollTo(0,0);  // scroll back to the top
                 resultString.setText("");  // clear the result field
+                resultString.setBackgroundColor(0xFFF3F3F3);
                 result = false;  // clear previous answer
                 break;
         }
     }
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        radioGrp.clearCheck();
-    }
 }
